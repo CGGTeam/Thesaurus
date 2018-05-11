9587c36f42bf07593410561cb74c1c29feacd3e2
von3007 <von3007@live.ca>
2018-05-10 23:02:51 -0400

    sounds fully working, seuleument besoin de call les fonctions aux bonnes endroit

--- scripts/Sounds.js
+++ scripts/Sounds.js
@@ -6,6 +6,11 @@ class Sounds {
         //initialise tout les audio files
         this.audioOuvrirMur = new Audio('docs/audio/ouvrirMur.wav');
         this.audioTresor = new Audio('docs/audio/tresor.wav');
+        this.audioGameOver = new Audio('docs/audio/gameOver.wav');
+        this.audioTeleport = new Audio('docs/audio/teleport.wav');
+        this.audioWin = new Audio('docs/audio/win.wav');
+        this.audioTimesUp = new Audio('docs/audio/timesUp.wav');
+        this.audioLevelStart = new Audio('docs/audio/levelStart.wav');
     }
     /**
      * retourne l'instance d'audio
@@ -15,18 +20,46 @@ class Sounds {
           Audio.instance = new Sounds();
         return Audio.instance;
     }
-    /**
-     * joue l'audio d'ouvrir un mur
-     */
     playOuvrirMur(){
         this.audioOuvrirMur.currentTime = 0;
         this.audioOuvrirMur.play();
     }
-    /**
-     * joue l'audio de ramasser le tresor
-     */
     playTresor(){
         this.audioTresor.currentTime = 0;
         this.audioTresor.play();
     }
+    playGameOver(){
+        this.audioGameOver.currentTime = 0;
+        this.audioGameOver.play();
+    }
+    playTeleport(){
+        this.audioTeleport.currentTime = 0;
+        this.audioTeleport.play();
+    }
+    playWin(){
+        this.audioWin.currentTime = 0;
+        this.audioWin.play();
+    }
+    playTimesUp(){
+        this.audioTimesUp.currentTime = 0;
+        this.audioTimesUp.play();
+    }
+    playLevelStart (){
+        this.audioLevelStart.currentTime = 0;
+        this.audioLevelStart.play();
+    }
+    /**
+     * Change le volume de tout les audios au float passé en argument
+     * @param {float} fltVolume 
+     */
+    changeVolume(fltVolume){
+        this.audioOuvrirMur.volume = fltVolume;
+        this.audioTresor.volume = fltVolume;
+        this.audioGameOver.volume = fltVolume;
+        this.audioTeleport.volume = fltVolume;
+        this.audioWin.volume = fltVolume;
+        this.audioTimesUp.volume = fltVolume;
+        this.audioLevelStart.volume = fltVolume;
+    }
+
 }
